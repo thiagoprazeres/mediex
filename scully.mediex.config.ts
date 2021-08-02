107363c0f60e5ecc2ee070445db653204b5eb5ec
Thiago Prazeres <thiagoprazeres@gmail.com>
2021-08-02 18:47:54 -0300

    title

--- scully.mediex.config.ts
+++ scully.mediex.config.ts
@@ -4,5 +4,21 @@ export const config: ScullyConfig = {
   projectName: "mediex",
   outDir: './dist/static',
   routes: {
+  },
+  puppeteerLaunchOptions: {
+    args: [
+      "--disable-gpu",
+      "--renderer",
+      "--no-sandbox",
+      "--no-service-autorun",
+      "--no-experiments",
+      "--no-default-browser-check",
+      "--disable-dev-shm-usage",
+      "--disable-setuid-sandbox",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process",
+      "--disable-extensions"
+    ]
   }
 };
